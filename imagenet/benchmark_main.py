4c0445ed03f131b76db56c47a909c28ca0ca68e8
cx2 <cx2@illinois.edu>
2019-12-11 16:50:13 -0600

    launch by ssh

--- imagenet/benchmark_main.py
+++ imagenet/benchmark_main.py
@@ -378,10 +378,10 @@ def train(train_loader, model, criterion, optimizer, epoch, log_writer):
                    epoch, i, len(train_loader), batch_time=batch_time,
                    data_time=data_time, loss=losses, top1=top1, top5=top5, train_time=train_record.get_time()))
             train_record.set()
-            
-        if i % args.print_freq == 0:
-            # debug
-            break
+
+        # if i % args.print_freq == 0:
+        #     # debug
+        #     break
 
     if log_writer:
         log_writer.add_scalar('train_iter/top1', top1.get_avg(), iter_ptr)
