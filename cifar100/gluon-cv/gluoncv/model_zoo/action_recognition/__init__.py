44e70c603b65619fc292905bd341cbc625f30f39
unknown <xcgoner1108@gmail.com>
2019-12-25 14:06:41 -0600

    no longer need mxnet implementation

--- cifar100/gluon-cv/gluoncv/model_zoo/action_recognition/__init__.py
+++
@@ -1,6 +0,0 @@
-# pylint: disable=wildcard-import
-"""Video action recognition."""
-from __future__ import absolute_import
-from .vgg16_ucf101 import *
-from .inceptionv3_ucf101 import *
-from .inceptionv3_kinetics400 import *
--- cifar100/mxnet/3rdparty/tvm/docker/install/ubuntu_install_golang.sh
+++
@@ -1,4 +0,0 @@
-#install the necessary dependancies for golang build
-apt-get update && apt-get install -y golang-0.10-go
-apt-get update && apt-get install -y godoc
-apt-get update && apt-get install -y golint
